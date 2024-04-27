0930c0c6013c92649efca24e096570cfb58b145a
Nikita Julian Bukowicz <4895384@teb-gdansk.ad>
2024-04-27 11:53:59 +0200

    Add Html and Handle

--- index.php
+++ index.php
@@ -4,10 +4,49 @@ declare(strict_types=1);
 
 namespace App;
 
-include_once('./src/debug.php');
+include_once('./src/utils/debug.php');
 
-dump('test');
-dump('+++ DUMP +++');
-
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = null;
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista notatek PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Moje notatki</h1>
+    </header>
+
+    <main>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Lista notatek</a>
+                </li>
+                <li>
+                    <a href="/?action=create">Nowa notatka</a>
+                </li>
+            </ul>
+        </nav>
+        <article>
+            <?php if ($action = 'create') : ?>
+                <h3>Nowa notatka</h3>
+                <?php else : ?>
+                        <h3>Lista notatek</h3>
+                <?php endif; ?>
+        </article>
+    </main>
+
+    <footer>Stopka</footer>
+
+</body>
+</html>
