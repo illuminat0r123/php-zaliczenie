b376c0f101f46941b3cb0b5506edaded6dc7f510
Nikita Julian Bukowicz <4895384@teb-gdansk.ad>
2024-04-28 12:22:47 +0200

    Add Exception

--- index.php
+++ index.php
@@ -7,13 +7,31 @@ namespace App;
 
 require_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
+require_once('./config/config.php');
+require_once('./Exception/AppException.php');
+require_once('./Exception/StorageException.php');
+require_once('./Exception/ConfigurationException.php');
 
-$request = [
-    'get' => $_GET,
-    'post' => $_POST,
-];
-$controller = new Controller($request);
-$controller->run();
+use App\Exception\AppException;
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
+
+try {
+    Controller::initConfiguration($configuration);
+    $request = [
+        'get' => $_GET,
+        'post' => $_POST,
+    ];
+    $controller = new Controller($request);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo "<h2>{$e->getMessage()}</h2>";
+} catch (Throwable $e) {
+    echo "<h1>Wystąpił błąd w aplikacji - skontaktuj się z administratorem</h1>";
+}
 
 ?>
 
