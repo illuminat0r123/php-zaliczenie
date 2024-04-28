fdc857a5d427e6657a51bd66860af85d991290df
Nikita Julian Bukowicz <4895384@teb-gdansk.ad>
2024-04-28 10:19:58 +0200

    created controller

--- index.php
+++ index.php
@@ -5,39 +5,11 @@ declare(strict_types=1);
 namespace App;
 
 
-require_once('./src/View.php');
+require_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 
-
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-
-switch($action) {
-    case 'create':
-        $page = 'create';
-        $created = false;
-            if(!empty($_POST)) {
-        $viewParams = [
-            'title' => $_POST['title'],
-            'description' => $_POST['description'],
-        ];
-        $created = true;
-        }
-        $viewParams['created'] = $created;
-        break;
-    default: 
-        $page = 'list';
-        $viewParams['resultList'] = 'Wyświetlamy listę notatek';
-
-}
-
-
-$view = new View();
-$view -> render($page, $viewParams);
+$controller = new Controller($_GET, $_POST);
+$controller->run();
 
 ?>
 
